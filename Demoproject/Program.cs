4aef638cc0c1af586dcdca4903099673733d461e
Devplay9847 <devendra.nishad.9847@gmail.com>
2022-04-27 20:36:35 +0530

    this is my first c#project

---
+++ Demoproject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demoproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello word");
+           // Console.ReadLine();
+            Program p = new Program();
+            p.printName();
+        }
+        
+    public void printName() {
+            Console.WriteLine("hello word");
+            Console.ReadLine();
+        }
+    }
+}
